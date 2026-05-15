c2430c3209b80c4b145a0d1c4952867439d7ef93
chentamil <chentamil1988@gmail.com>
2026-05-15 20:25:01 +0530

    pickle ball sports,all working but admin err401 me.js v1.021

--- functions/api/me.js
+++ functions/api/me.js
@@ -1,19 +1,21 @@
 export async function onRequest(context) {
 
-  const cookie = context.request.headers.get("Cookie") || "";
+  const cookie =
+    context.request.headers.get("Cookie") || "";
 
-  console.log("COOKIE HEADER:", cookie);
-
-  const tokenMatch = cookie.match(/sb_access_token=([^;]+)/);
+  const tokenMatch =
+    cookie.match(/sb_access_token=([^;]+)/);
 
   if (!tokenMatch) {
-    return new Response(JSON.stringify({
-      error: "No cookie found"
-    }), { status: 401 });
+
+    return new Response("Unauthorized", {
+      status: 401
+    });
+
   }
 
   return Response.json({
-    authenticated: true,
-    tokenPreview: tokenMatch[1].slice(0, 10)
+    authenticated: true
   });
+
 }
